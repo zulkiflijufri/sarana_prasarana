7177a3edbf809a84433752337abb3387740125f8
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-20 21:54:11 +0700

    Update route web

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@
 // });
 
 Route::get('/', 'AuthController@login')->name('login')->middleware('guest');
-//Route::get('/login', 'AuthController@login')->name('login');
 Route::post('/postlogin', 'AuthController@postlogin');
 Route::get('/logout', 'AuthController@logout');
 
