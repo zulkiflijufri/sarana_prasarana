17e2f1f14549d6d6c89f676e8c0a29287d03a8f0
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-21 22:36:01 +0700

    Modify title dasboard

--- resources/views/dasboard.blade.php
+++ resources/views/dasboard.blade.php
@@ -7,7 +7,7 @@
 		<div class="main-content">
 			<div class="row text-center">
 				<div class="col-lg-12">
-					<h3>Hi {{auth()->user()->name}}, Selamat Datang Di Aplikasi Sarana Dan Prasana</h3>
+					<h3>Hi {{auth()->user()->name}}, Selamat Datang Di Aplikasi Sarana Dan Prasarana</h3>
 				</div>	
 			</div>
 		</div>
