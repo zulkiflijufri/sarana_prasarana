6e7411583c793117298c614c5dd71505504a44f4
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-21 22:01:18 +0700

    Modify routes web

--- routes/web.php
+++ routes/web.php
@@ -11,37 +11,28 @@
 |
 */
 
-// Route::get('/', function () {
-// 	return view('welcome');
-// });
-
 Route::get('/', 'AuthController@login')->name('login')->middleware('guest');
 Route::post('/postlogin', 'AuthController@postlogin');
 Route::get('/logout', 'AuthController@logout');
 
+// Role admin,atasan,pengajuan
 Route::group(['middleware' => ['auth', 'checkRole:admin,atasan,pengajuan']], function() {
 	Route::get('/dasboard', 'DasboardController@index');
 	Route::get('/history', 'PersetujuanController@history');
 	Route::get('/pdf_persetujuan/{id}', 'PersetujuanController@pdf_persetujuan');
 });
 
-Route::group(['middleware' => ['auth', 'checkRole:admin']], function() {
+// Role admin dan pengajuan
+Route::group(['middleware' => ['auth', 'checkRole:admin,pengajuan']], function() {
 	Route::get('/pengajuan', 'PengajuanController@index');
 	Route::post('/pengajuan/create', 'PengajuanController@create');
-	Route::get('/persetujuan', 'PersetujuanController@index');
-	Route::get('/persetujuan/detail/{id}', 'PersetujuanController@detail');
-	Route::post('/persetujuan/proses/{id}', 'PersetujuanController@proses');
-	Route::get('/persetujuan/delete/{id}', 'PersetujuanController@delete');
 });
 
+// Role admin dan atasan
 Route::group(['middleware' => ['auth', 'checkRole:admin,atasan']], function() {
 	Route::get('/persetujuan', 'PersetujuanController@index');
 	Route::get('/persetujuan/detail/{id}', 'PersetujuanController@detail');
 	Route::post('/persetujuan/proses/{id}', 'PersetujuanController@proses');
-});
-
-Route::group(['middleware' => ['auth', 'checkRole:admin,pengajuan']], function() {
-	Route::get('/pengajuan', 'PengajuanController@index');
-	Route::post('/pengajuan/create', 'PengajuanController@create');
+	Route::get('/persetujuan/delete/{id}', 'PersetujuanController@delete');
 });
 
