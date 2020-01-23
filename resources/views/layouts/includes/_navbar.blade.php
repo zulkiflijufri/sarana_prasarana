be831e01f7fb3229d4014d7b4ebf8ccf910410e9
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-23 12:51:16 +0700

    Add form search

--- resources/views/layouts/includes/_navbar.blade.php
+++ resources/views/layouts/includes/_navbar.blade.php
@@ -1,11 +1,21 @@
 <nav class="navbar navbar-default navbar-fixed-top">
 			<div class="brand">
-				<a href="/dasboard" style="font-size: 19px">Sarana Prasana STT NF</a>
+				<a href="/dasboard" style="font-size: 19px">Sarana Prasarana STT NF</a>
 			</div>
 			<div class="container-fluid">
 				<div class="navbar-btn">
 					<button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
 				</div>
+				@if(request()->path() != 'dasboard' && request()->path() != 'pengajuan')
+					<form class="navbar-form navbar-left" action="{{'persetujuan' == request()->path() ? '/persetujuan' : '/history'}}" method="GET">
+						<div class="input-group">
+							<input type="text" class="form-control" name="cari" placeholder="{{'persetujuan' == request()->path() ? 'Cari persetujuan' : 'Cari history'}}" autocomplete="off">
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-primary">Cari</button>
+							</span>
+						</div>
+					</form>
+				@endif
 				<div id="navbar-menu">
 					<ul class="nav navbar-nav navbar-right">
 						<li class="dropdown">
