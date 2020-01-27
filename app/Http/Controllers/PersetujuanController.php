180733a885f8ef78ee40a13daf5619598dfb66f7
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-27 20:14:22 +0700

    Add logic form search and modify pdf pada pengajuan

--- app/Http/Controllers/PersetujuanController.php
+++ app/Http/Controllers/PersetujuanController.php
@@ -12,17 +12,20 @@ class PersetujuanController extends Controller
     public function index(Request $request) {
         if ($request->has('cari')) {
             $pengajuans = Pengajuan::where('nama_pengajuan', 'LIKE', '%'.$request->cari.'%')
-                          ->orderBy('id','DESC')->paginate(5);
+            ->orWhere('unit', 'LIKE', '%'.$request->cari.'%')->orWhere('waket_satker', 'LIKE',
+            '%'.$request->cari.'%')->orderBy('id','DESC')->paginate(5);
         } else {
-            $pengajuans = Pengajuan::orderBy('id', 'DESC')->paginate(5);    
+            $pengajuans = Pengajuan::orderBy('id', 'DESC')->paginate(5);
         }
-        return view ('persetujuan.index', ['pengajuans' => $pengajuans]);
+        return view ('persetujuan.index', compact('pengajuans'));
     }
 
     public function detail(Request $request, $id) {
         $pengajuan = Pengajuan::findOrFail($id);
         $barangs = Barang::where('pengajuan_id', $id)->get();
-        return view ('persetujuan.detail', compact('barangs','pengajuan'));
+        $status_barang = Barang::where('pengajuan_id', $id)->whereIn('status', ['Ya', 'Tidak'])->get();
+        $jml_status = count($status_barang);
+        return view ('persetujuan.detail', compact('barangs','pengajuan','jml_status'));
     }
 
     public function delete($id) {
@@ -53,7 +56,9 @@ class PersetujuanController extends Controller
     {
         if ($request->has('cari')) {
             $pengajuans = Pengajuan::where('perihal', 'LIKE', '%'.$request->cari.'%')
-                          ->orderBy('id','DESC')->paginate(8);
+            ->orWhere('unit', 'LIKE', '%'.$request->cari.'%')->orWhere('waket_satker', 'LIKE',
+            '%'.$request->cari.'%')->orWhere('tanggal', 'LIKE', '%'.$request->cari.'%')
+            ->orderBy('id','DESC')->paginate(8);
         } else {
             $pengajuans = Pengajuan::orderBy('id', 'DESC')->paginate(8);
         }
@@ -63,8 +68,10 @@ class PersetujuanController extends Controller
     public function pdf_persetujuan($id)
     {
         $pengajuan = Pengajuan::findOrFail($id);
-        $barangs = Barang::where('pengajuan_id', $id)->whereIn('status', ['Ya'])->get();
-        $pdf = PDF::loadview('persetujuan.pdf_persetujuan', ['pengajuan' => $pengajuan], compact('barangs'));
+        $barangs = Barang::where('pengajuan_id', $id)->get();
+        $acc_barangs = Barang::where('pengajuan_id', $id)->whereIn('status', ['Ya'])->get();
+        $jml_status = count($acc_barangs);
+        $pdf = PDF::loadview('persetujuan.pdf_persetujuan',compact('pengajuan','barangs','acc_barangs','jml_status'));
         return $pdf->stream('Pengajuan Barang_'.$pengajuan->nama_pengajuan.'.pdf');
     }
 }
