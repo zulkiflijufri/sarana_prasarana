e1156387fb526b7b80c7ff8064b0115015f54df6
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-27 20:10:12 +0700

    Add Model Pengajuan dan Barang

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Pengajuan;
+use App\Barang;
 use Illuminate\Http\Request;
 
 class DasboardController extends Controller
 {
     public function index() {
-        return view('dasboard');
+    	$pengajuan = Pengajuan::all();
+    	$barang = Barang::all();
+    	$no_proses = Barang::where('status', null)->get();
+    	$barang_acc = Barang::where('status', 'Ya')->get();
+        return view('dasboard',compact('pengajuan','no_proses','barang','barang_acc'));
     }
 }
