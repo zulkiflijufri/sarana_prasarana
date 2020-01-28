4ba29e4a8a8735973d6756f80adf07a808fb5a2a
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-29 00:57:20 +0700

    Comment jquery 3.3.1

--- resources/views/pengajuan/index.blade.php
+++ resources/views/pengajuan/index.blade.php
@@ -165,7 +165,7 @@
 	
 	<script src="{{asset('admin/assets/vendor/jquery/jquery.min.js')}}"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
 	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script> -->
 	
