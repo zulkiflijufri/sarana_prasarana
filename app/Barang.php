35ff3e0d2f4fce01cbf781b406320344c18ffaf3
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-01-29 00:25:53 +0700

    Modify model

--- app/Barang.php
+++ app/Barang.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     protected $table = "barang";
-    protected $fillable = ['nama_barang', 'link_gambar', 'quantity', 'satuan_barang','harga_satuan', 'jumlah', 'status', 'pengajuan_id'];
+    protected $fillable = 
+    [
+    	'nama_barang', 'link_gambar', 
+    	'quantity', 'satuan_barang',
+    	'harga_satuan', 'jumlah', 
+    	'status', 'pengajuan_id'
+    ];
 
     public function pengajuan()
     {
