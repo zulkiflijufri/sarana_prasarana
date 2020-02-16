bf16a38ac7d3b13a316288a93da5835e87fede18
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-02-17 06:41:45 +0700

    Add field proses

--- app/Pengajuan.php
+++ app/Pengajuan.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pengajuan extends Model
 {
     protected $table = "pengajuan";
-    protected $fillable = ['nama_pengajuan', 'unit', 'waket_satker', 'perihal', 'tanggal' ,'catatan', 'total_harga'];
+    protected $fillable = ['nama_pengajuan', 'unit', 'waket_satker', 'perihal', 'tanggal' ,'catatan', 'total_harga','proses'];
 
     public function barang() {
     	return $this->hasMany(Barang::class);
