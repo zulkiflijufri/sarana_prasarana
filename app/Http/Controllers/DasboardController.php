2174bd4d5d563e64d40d7e039e101eea92683065
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-02-17 06:41:05 +0700

    Change model barang to pengajuan

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -11,7 +11,7 @@ class DasboardController extends Controller
     public function index() {
     	$pengajuan = Pengajuan::all();
     	$barang = Barang::all();
-    	$no_proses = Barang::where('status', null)->get();
+    	$no_proses = Pengajuan::where('proses', 'Belum')->get();
     	$barang_acc = Barang::where('status', 'Ya')->get();
         return view('dasboard',compact('pengajuan','no_proses','barang','barang_acc'));
     }
