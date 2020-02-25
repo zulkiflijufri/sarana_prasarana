0aa3b5f42b15646ead3495223cb5c182755be49b
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-02-25 22:15:27 +0700

    Get data Pengajuan where unit = STT NF and NF Komputer

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -11,8 +11,10 @@ class DasboardController extends Controller
     public function index() {
     	$pengajuan = Pengajuan::all();
     	$barang = Barang::all();
+    	$sttnf = Pengajuan::where('unit','STT NF')->get();
+    	$nfcom = Pengajuan::where('unit','NF Komputer')->get();
     	$no_proses = Pengajuan::where('proses', 'Belum')->get();
     	$barang_acc = Barang::where('status', 'Ya')->get();
-        return view('dasboard',compact('pengajuan','no_proses','barang','barang_acc'));
+        return view('dasboard',compact('pengajuan','no_proses','barang','barang_acc','sttnf','nfcom'));
     }
 }
