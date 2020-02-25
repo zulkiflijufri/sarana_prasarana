2029e9bef7a0444ad5910cf549d8728e92fbbfad
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-02-25 22:07:06 +0700

    Clean code sidebar

--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -3,7 +3,7 @@
 				<nav>
 					<ul class="nav list-group">
 						@if(auth()->user()->role == 'admin')
-							<li><a href="/dasboard" style="font-size: 15px" class="{{ 'dasboard' == request()->path() ? 'active' : ''}}"><i class="lnr lnr-home"></i> <span>Dasboard</span></a></li>
+							<li><a href="/dasboard" style="font-size: 15px" class="{{ 'dasboard' == request()->path() ? 'active' : ''}}"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
 							<li><a href="/pengajuan" style="font-size: 15px" class="{{ 'pengajuan' == request()->path() ? 'active' : ''}}"><i class="lnr lnr-pencil"></i> <span>Pengajuan</span></a></li>
 							<li><a href="/persetujuan" style="font-size: 15px" class="{{ 'persetujuan' == request()->path() ? 'active' : ''}}"><i class="lnr lnr-envelope"></i> <span>Persetujuan</span></a></li>
 							<li><a href="/history" style="font-size: 15px" class="{{ 'history' == request()->path() ? 'active' : ''}}"><i class="lnr lnr-chart-bars"></i> <span>History Pengajuan</span></a></li>
