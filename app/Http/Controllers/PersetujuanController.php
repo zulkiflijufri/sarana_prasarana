bacd525b9988d0b70648d4a252dbfc8520bc6221
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-03-08 22:52:50 +0700

    Clean Code

--- app/Http/Controllers/PersetujuanController.php
+++ app/Http/Controllers/PersetujuanController.php
@@ -2,48 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Pengajuan;
-use App\Barang;
 use PDF;
+use App\Barang;
+use App\Pengajuan;
+use Illuminate\Http\Request;
 
 class PersetujuanController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         if ($request->has('cari')) {
-            $pengajuans = Pengajuan::where('nama_pengajuan', 'LIKE', '%'.$request->cari.'%')
-            ->orWhere('unit', 'LIKE', '%'.$request->cari.'%')->orWhere('waket_satker', 'LIKE',
-            '%'.$request->cari.'%')->simplePaginate(5);
+            $pengajuans = Pengajuan::where('nama_pengajuan', 'LIKE', '%' . $request->cari . '%')
+                          ->orWhere('unit', 'LIKE', '%' . $request->cari . '%')->orWhere('waket_satker','LIKE','%' . $request->cari . '%')
+                          ->simplePaginate(5);
         } else {
             $pengajuans = Pengajuan::simplePaginate(5);
         }
-        return view ('persetujuan.index', compact('pengajuans'));
+        return view('persetujuan.index', compact('pengajuans'));
     }
 
-    public function detail(Request $request, $id) {
+    public function detail(Request $request, $id)
+    {
         $pengajuan = Pengajuan::findOrFail($id);
         $barangs = Barang::where('pengajuan_id', $id)->get();
         $status_barang = Barang::where('pengajuan_id', $id)->whereIn('status', ['Ya', 'Tidak'])->get();
-        return view ('persetujuan.detail', compact('barangs','pengajuan','status_barang'));
+        return view('persetujuan.detail', compact('barangs', 'pengajuan', 'status_barang'));
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $pengajuan = Pengajuan::find($id);
         $pengajuan->delete();
         return redirect('/persetujuan')->with('hapus', 'Data Pengajuan Berhasil Di Hapus');
     }
 
-    public function proses(Request $request ,$id)
+    public function proses(Request $request, $id)
     {
         $status = $request->status;
         $nama_barang = $request->nama_barang;
-        $barang = Barang::where('pengajuan_id',$id)->get();
+        $barang = Barang::where('pengajuan_id', $id)->get();
 
         for ($i = 0; $i < count($nama_barang); $i++) {
             Barang::where([
+                ['id', '=', $barang[$i]->id],
                 ['nama_barang', '=', $barang[$i]->nama_barang],
-                ['pengajuan_id', '=',$barang[0]->pengajuan_id]])->update([
-                    'status' => $status[$i]
+                ['pengajuan_id', '=', $barang[0]->pengajuan_id]
+            ])->update([
+                'status' => $status[$i]
             ]);
         }
 
@@ -59,22 +64,23 @@ class PersetujuanController extends Controller
     public function history(Request $request)
     {
         if ($request->has('cari')) {
-            $pengajuans = Pengajuan::where('perihal', 'LIKE', '%'.$request->cari.'%')
-            ->orWhere('unit', 'LIKE', '%'.$request->cari.'%')->orWhere('waket_satker', 'LIKE',
-            '%'.$request->cari.'%')->orWhere('tanggal', 'LIKE', '%'.$request->cari.'%')
-            ->simplePaginate(8);
+            $pengajuans = Pengajuan::where('perihal', 'LIKE', '%' . $request->cari . '%')
+                        ->orWhere('unit', 'LIKE', '%' . $request->cari . '%')->orWhere('waket_satker','LIKE','%' . $request->cari . '%')
+                        ->orWhere('tanggal', 'LIKE', '%' . $request->cari . '%')
+                        ->simplePaginate(8);
         } else {
             $pengajuans = Pengajuan::simplePaginate(5);
         }
-        return view ('persetujuan.history', ['pengajuans' => $pengajuans]);
+        return view('persetujuan.history', ['pengajuans' => $pengajuans]);
     }
 
     public function pdf_persetujuan($id)
     {
         $pengajuan = Pengajuan::findOrFail($id);
         $barangs = Barang::where('pengajuan_id', $id)->get();
+        $no_barang = Barang::where('pengajuan_id', $id)->whereIn('status', ['Tidak'])->get();
         $acc_barang = Barang::where('pengajuan_id', $id)->whereIn('status', ['Ya'])->get();
-        $pdf = PDF::loadview('persetujuan.pdf_persetujuan',compact('pengajuan','barangs','acc_barang'));
-        return $pdf->stream('Pengajuan Barang_'.$pengajuan->nama_pengajuan.'.pdf');
+        $pdf = PDF::loadview('persetujuan.pdf_persetujuan', compact('pengajuan', 'barangs', 'acc_barang', 'no_barang'));
+        return $pdf->stream('Pengajuan Barang_' . $pengajuan->nama_pengajuan . '.pdf');
     }
 }
